65be39471b9bc38e173c120ec7162d4715ba1f27
junblee <topguide82@gmail.com>
2019-07-11 16:25:50 +0900

    Servlet

--- aboutme/src/exam/TodayServlet.java
+++ aboutme/src/exam/TodayServlet.java
@@ -19,18 +19,11 @@ import javax.servlet.http.HttpServletResponse;
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TodayServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-    
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("text/html");
