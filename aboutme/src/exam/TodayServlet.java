f6c8c9d1332d4ede052a0416fadc7b90b521d933
junblee <topguide82@gmail.com>
2019-07-15 11:57:43 +0900

    Code Review
    aboutme.html
    body > div > header > input.nav_button onClick link is adjusted
    
    index.html
    body > div > header > input.nav_button onClick link is adjusted to
    absolute path
    
    
    photo.html
    body > div > header > input.nav_button onClick link is added
    'location.href'
    
    TodayServlet.java
    - remove unnecessary comments
    - Delete unnecessary constructor.
    - out.write('relative path')
    - apply GoogleStyle fomatter

--- aboutme/src/exam/TodayServlet.java
+++ aboutme/src/exam/TodayServlet.java
@@ -12,32 +12,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TodayServlet
- */
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public TodayServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.setContentType("text/html");
-		request.setCharacterEncoding("utf8"); 
-		response.setCharacterEncoding("utf8"); 
-		PrintWriter out = response.getWriter(); 
-		out.write("<a href='http://127.0.0.1:5500/WebContent/index.html'>메인화면</a><br>");
-		out.write("<a href='http://127.0.0.1:5500/WebContent/index.html'>메인화면</a><br>");
 
-		String local_time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("현재시간 : yyyy/MM/dd HH:mm", Locale.KOREA));
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		request.setCharacterEncoding("utf8");
+		response.setCharacterEncoding("utf8");
+		PrintWriter out = response.getWriter();
+		out.write("<a href='index.html'>메인화면</a><br>");
+
+		String localTime = LocalDateTime.now()
+				.format(DateTimeFormatter.ofPattern("현재시간 : yyyy/MM/dd HH:mm", Locale.KOREA));
 		out.write("<html>");
 		out.write("<body>");
 		out.write("<h1>");
-		out.write(local_time);
+		out.write(localTime);
 		out.write("</h1>");
 		out.write("</body>");
 		out.write("</html>");
